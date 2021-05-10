da1248a3aaa2f69b994cd7710ebcefea34346534
soundara rajan <soundaraj.thevan@gmail.com>
2021-05-10 20:18:36 +0530

    Update App.js

--- App.js
+++ App.js
@@ -8,15 +8,7 @@ import {
 import { createStore } from 'redux'
 import CounterApp from './src/CounterApp'
 import { Provider } from 'react-redux'
-/**
- * Store - holds our state - THERE IS ONLY ONE STATE 
- * Action - State can be modified using actions - SIMPLE OBJECTS 
- * Dispatcher - Action needs to be sent by someone - known as dispatching an action
- * Reducer - receives the action and modifies the state to give us a new state 
- *  - pure functions 
- *  - only mandatory argument is the 'type' 
- * Subscriber - listens for state change to update the ui  
- */
+
 const initialState = {
     counter: 0
 }
